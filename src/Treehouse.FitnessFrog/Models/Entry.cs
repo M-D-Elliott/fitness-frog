70321064eb217eda70108d42206b7de2c64f679c
M-D-Elliott <40653200+M-D-Elliott@users.noreply.github.com>
2019-03-05 16:18:35 -0600

    Added  server-side validation rules using data annotations

--- src/Treehouse.FitnessFrog/Models/Entry.cs
+++ src/Treehouse.FitnessFrog/Models/Entry.cs
@@ -89,6 +89,7 @@ namespace Treehouse.FitnessFrog.Models
         /// <summary>
         /// The notes for the entry.
         /// </summary>
+        [MaxLength(200, ErrorMessage = "Notes must be 200 characters or fewer.")]
         public string Notes { get; set; }
     }
 }
